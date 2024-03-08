65a9c032d60ee644e01a3ea5f4e9fb290aafa1f5
Anmol Katakwar <anmolkatakwar99@gmail.com>
2024-03-08 21:57:59 +0530

    portfolio update

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,19 @@
 import './portfolio.scss'
 import PortfolioList from "../portfolioList/PortfolioList";
-import {  useState } from "react";
+import {  useEffect, useState } from "react";
+import {
+  featuredPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  designPortfolio,
+  contentPortfolio,
+} from "../../data";
 
 export const Portfolio = () => {
 
   const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
+
 
   const list = [
     {
@@ -28,6 +37,30 @@ export const Portfolio = () => {
       title: "Content",
     },
   ];
+
+  useEffect(()=>{
+
+    switch (selected) {
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+      case "web":
+        setData(webPortfolio);
+        break;
+      case "mobile":
+        setData(mobilePortfolio);
+        break;
+      case "design":
+        setData(designPortfolio);
+        break;
+      case "content":
+        setData(contentPortfolio);
+        break;
+      default:
+        setData(featuredPortfolio);
+    }
+  },[selected])
+
   return (
     <div className='portfolio' id = "portfolio">
       <h1>Portfolio</h1>
@@ -43,30 +76,15 @@ export const Portfolio = () => {
         ))}
       </ul>
       <div className="container">
-        <div className="item">
-          <img src="assets/chat_app.png" alt="" />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img src="assets/chat_app.png" alt="" />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img src="assets/chat_app.png" alt="" />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img src="assets/chat_app.png" alt="" />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img src="assets/chat_app.png" alt="" />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img src="assets/chat_app.png" alt="" />
-          <h3>Banking App</h3>
-        </div>
+      {data.map((d) => (
+          <div className="item">
+            <img
+              src={d.img}
+              alt=""
+            />
+            <h3>{d.title}</h3>
+          </div>
+        ))}
       </div>
     </div>
   )
